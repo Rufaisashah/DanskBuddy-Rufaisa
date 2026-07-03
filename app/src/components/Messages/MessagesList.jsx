10bb1006dde301aa1b60c24fa16eb1b8b8ee266a
Jyoti <switjyoti26~@gmail.com>
2026-07-03 11:26:06 +0200

    fix: improve the UI

--- app/src/components/Messages/MessagesList.jsx
+++ app/src/components/Messages/MessagesList.jsx
@@ -22,11 +22,7 @@ export default function MessagesList({ conversations, currentUserId }) {
             onClick={() => navigate(`/messages/${conv.otherUser.id}`)}
             className={`
   flex items-center gap-3 px-4 py-3 cursor-pointer transition-all
-  ${
-    isActive
-      ? "bg-[#FDEAEC] rounded-2xl mx-3 my-2"
-      : "hover:bg-gray-50 rounded-2xl mx-3 my-2"
-  }
+  ${isActive ? "bg-[#FDEAEC] rounded-2xl mx-3 my-2" : "rounded-2xl mx-3 my-2"}
 `}
           >
             <Avatar
